0d3ab5ba872756db305a275e655723362c554115
soyRicardoCastro <ricardocastro.dev@gmail.com>
2022-07-03 18:51:48 +0200

    Remove functions zzz

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,4 +1,6 @@
 import UserModel, { User } from "../model/user.model";
+import UniversityModel, { University } from "../model/university.model"
+import { DocumentType } from "@typegoose/typegoose";
 
 export function createUser(input: Partial<User>) {
   return UserModel.create(input);
@@ -14,12 +16,4 @@ export function findUserByEmail(email: string) {
 
 export function findUsers() {
   return UserModel.find()
-}
-
-export function userUniversityList(id: string, uid: string) {
-  const user = UserModel.findByIdAndUpdate(id, {
-
-  })
-
-
 }
