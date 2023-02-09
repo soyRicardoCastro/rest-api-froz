2becf54400234bea2091ab455fbabef20109c2c5
JapCode <japcrowley.thetraveler@gmail.com>
2023-02-09 16:29:43 -0400

    remove test custom enviroment variabes

--- config/default.ts
+++ config/default.ts
@@ -7,7 +7,4 @@ export default {
   logLevel: "info",
   accessTokenPrivateKey: process.env.ACCESS_TOKEN_PRIVATE_KEY,
   refreshTokenPrivateKey: process.env.REFRESH_PRIVATE_KEY,
-  //test
-  accessTokenPublicKey: process.env.ACCESS_TOKEN_PUBLIC_KEY,
-  refreshTokenPublicKey: process.env.REFRESH_PUBLIC_KEY,
 };
